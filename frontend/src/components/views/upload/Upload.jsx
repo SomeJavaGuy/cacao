00224b45e83a79a75c8c77c9dde7d39c026c9936
jdollarz <john.asafo@gmail.com>
2021-03-21 01:18:28 +0000

    JA - hero  changes 5

--- frontend/src/components/views/upload/Upload.jsx
+++ frontend/src/components/views/upload/Upload.jsx
@@ -1,15 +1,31 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { Col, Container, Row } from "reactstrap";
+import { Col, Container, Form, FormGroup, Input, Label, Row } from "reactstrap";
+
+const apiKey = "943a22605f49d4220dee";
+const apiSecret =
+  "ee0a4a944bddade87da802b4bfbcda38d32d1054474321bf15c13793c8d32a2c";
+
+// const postMetadata = (external_url) => {
+//   const metaUrl = 'https://api.pinata.cloud/pinning/pinJsonToIPFS';
+//   return axios
+//     .post(metaUrl, data, {
+//       maxContentLength: "Infinity", //this is needed to prevent axios from erroring out with large files
+//       headers: {
+//         "Content-Type": `multipart/form-data; boundary=${data._boundary}`,
+//         pinata_api_key: apiKey,
+//         pinata_secret_api_key: apiSecret,
+//       },
+//     })
+//     .then(function (response) {});
+// };
 
 export const Upload = () => {
   const [selectedFile, setSelectedFile] = useState();
   const [isFilePicked, setIsFilePicked] = useState(false);
+  const [isSpinning, setIsSpinning] = useState(false);
 
   const url = "https://api.pinata.cloud/pinning/pinFileToIPFS";
-  const apiKey = "943a22605f49d4220dee";
-  const apiSecret =
-    "ee0a4a944bddade87da802b4bfbcda38d32d1054474321bf15c13793c8d32a2c";
 
   const changeHandler = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -21,7 +37,7 @@ export const Upload = () => {
 
     // formData.append("File", selectedFile); // this was wrong
     data.append("file", selectedFile, selectedFile.name);
-
+    setIsSpinning(true);
     return axios
       .post(url, data, {
         maxContentLength: "Infinity", //this is needed to prevent axios from erroring out with large files
@@ -31,9 +47,25 @@ export const Upload = () => {
           pinata_secret_api_key: apiSecret,
         },
       })
-      .then(function (response) {
+      .then((response) => {
         //handle response here
+        // setIsSpinning(false);
         console.log("====>", response);
+
+        const external_url = response.data.IpfsHash;
+
+        console.log("external_url", external_url);
+
+        //  await postMetadata(external_url);
+
+        setIsSpinning(false);
+
+        // "name": /* NFT Name - This must be a string */,
+        // "description": /* Description of the NFT - This must be a string */,
+        // "image": /*  IPFS Hash to our content, this must be prefixed with "ipfs://ipfs/{{ IPFS_HASH ))" - This must be a string */,
+        // "external_url": /* This is the link to Rarible which we currently don't have, we can fill this in shortly */,
+        // "animation_url": /* IPFS Hash just as image field, but it allows every type of multimedia files. Like mp3, mp4 etc */,
+        // // the below section is not needed.
       })
       .catch(function (error) {
         console.log("====>", error);
@@ -47,6 +79,42 @@ export const Upload = () => {
           <Col>
             <h1>upload xx...</h1>;
             <div>
+              <Form>
+                <FormGroup>
+                  <Label for="name">Name</Label>
+                  <Input
+                    type="text"
+                    name="name"
+                    placeholder="enter asset name"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="description">Description</Label>
+                  <Input
+                    type="text"
+                    name="description"
+                    placeholder="enter description"
+                  />
+                </FormGroup>
+
+                {/* <FormGroup>
+                  <Label for="external_url">External URL</Label>
+                  <Input
+                    type="text"
+                    name="external_url"
+                    placeholder="enter external url"
+                  />
+                </FormGroup> */}
+
+                <FormGroup>
+                  <Label for="animation_url">Description</Label>
+                  <Input
+                    type="text"
+                    name="description"
+                    placeholder="enter description"
+                  />
+                </FormGroup>
+              </Form>
               <input type="file" name="file" onChange={changeHandler} />
               {isFilePicked ? (
                 <div>
@@ -65,6 +133,7 @@ export const Upload = () => {
                 <button onClick={handleSubmission}>Submit</button>
               </div>
             </div>
+            {isSpinning ? <p>Uploading...</p> : null}
           </Col>
         </Row>
       </Container>
